ba87e3838459fb405e628328a8846aaa8a375d66
César Rodríguez <kesarr@gmail.com>
2012-11-27 21:10:11 +0100

    Examples and duckboard lib basic functionality

--- duckboard.php
+++ duckboard.php
@@ -1,29 +1,29 @@
 <?php
 /**
  * class to connect with Ducksboard http://www.ducksboard.com
+ * @author César Rodríguez <kesarr@gmail.com>
  */
-
 class duckboard
 {
     const API_KEY = 'xn325IdvBu16B3CvmhHkaeHCuhLnOXvXkaEPvx73joGnA7j1TF';
 
     /**
      * @param int|string $widgetCode
-     * @param int|string $value
-     * @param int|null $delta
-     * @param string|null $timestamp
+     * @param int|string|array $value
      * @example curl - v - u APIKEY:x - d '{"delta": 5}' https://push.ducksboard.com/values/235
      */
-    public static function send($widgetCode, $value, $delta = null, $timestamp = null)
+    public static function send($widgetCode, $value)
     {
+        if (is_numeric($value)) {
+            $value = array('value' => $value);
+        }
         $curlRequest = curl_init('https://push.ducksboard.com/v/' . $widgetCode);
         curl_setopt($curlRequest, CURLOPT_USERPWD, self::API_KEY . ':ignored');
-        curl_setopt($curlRequest, CURLOPT_POSTFIELDS, json_encode(array('value' => $value)));
+        curl_setopt($curlRequest, CURLOPT_POSTFIELDS, json_encode($value));
         curl_setopt($curlRequest, CURLOPT_POST, 1);
+        curl_setopt($curlRequest, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curlRequest, CURLOPT_RETURNTRANSFER, true);
         curl_exec($curlRequest);
+        curl_close($curlRequest);
     }
 }
-
-duckboard::send('test1', 7);
-duckboard::send('test2', 5);
---
+++ example.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * example to use PHPDucksboard
+ * @author César Rodríguez <kesarr@gmail.com>
+ */
+require_once 'duckboard.php';
+
+/*
+duckboard::send('num_cases_ic', 21);
+duckboard::send('num_cases_ate', 42);
+duckboard::send('num_physician', 26);
+duckboard::send('addendum_percentage', 0.4);
+$data = array(
+    'timestamp' => time(),
+    'value' => array(
+        'title' => 'new case',
+        'image' => 'https://app.ducksboard.com/static/img/timeline/green.gif',
+        'content' => 'A new case has been created by Intake'
+    )
+);
+duckboard::send('timeline', $data);
+*/
+
